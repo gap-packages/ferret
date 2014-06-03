57c4af9d3f24a3c2006f486c249a90d27a0d4d96
Chris Jefferson <caj21@st-andrews.ac.uk>
2014-06-03 20:45:12 +0100

    Switch to faster hash

--- YAPB++/source/library/hash.hpp
+++ YAPB++/source/library/hash.hpp
@@ -1,7 +1,7 @@
 #ifndef QUICK_HASH_HPP_YIA
 #define QUICK_HASH_HPP_YIA
 
-#if 1
+#if 0
 
 #include "fnv_hash.hpp"
 
@@ -11,6 +11,7 @@
 
 u_int64_t quick_hash(u_int64_t key)
 {
+    //return key + key*key;
   key = (~key) + (key << 21); // key = (key << 21) - key - 1;
   key = key ^ (key >> 24);
   key = (key + (key << 3)) + (key << 8); // key * 265
@@ -23,12 +24,14 @@ u_int64_t quick_hash(u_int64_t key)
 
 u_int64_t inline hash_combine(u_int64_t val1, u_int64_t val2)
 {
-    return 15485863 * val1 + 19661 * val2;
+    return (u_int64_t)15485863 * val1 + (u_int64_t)19661 * val2;
 }
 
 u_int64_t inline hash_combine(u_int64_t val1, u_int64_t val2, u_int64_t val3)
 {
-    return 15485863 * val1 + 19661 * val2 + 22801763489 * val3;
+    return (u_int64_t)15485863 * val1 +
+           (u_int64_t)19661 * val2 +
+           (u_int64_t)22801763489 * val3;
 }
 #endif
 
