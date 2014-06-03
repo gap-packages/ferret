60b4d242fb2e6c9ea5404e724a9fce4411e4213e
Chris Jefferson <caj21@st-andrews.ac.uk>
2014-06-03 21:05:40 +0100

    Speed up tests

--- YAPB++/tests/do_single_test.sh
+++ YAPB++/tests/do_single_test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 . ../../ferret.vars
 
-$CXX  $1 -I ../source $DEBUGFLAGS -Wall -Wextra -g -o $1.exe && $VALGRIND ./$1.exe && rm $1.exe
-
+$CXX $1 -I ../source $DEBUGFLAGS -Wall -Wextra -std=gnu++11  -o $1.exe && $VALGRIND ./$1.exe && rm -r $1.exe*
