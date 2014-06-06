1cd2349d36c5dd50f93c20787adcb724f2b5bf86
Chris Jefferson <caj21@st-andrews.ac.uk>
2014-06-06 10:56:39 +0100

    Always enable C++11 (part 2)

--- YAPB++/source/library/cpp11.hpp
+++ YAPB++/source/library/cpp11.hpp
@@ -1,10 +1,6 @@
 #ifndef CPP11_CAJ_HEADER
 #define CPP11_CAJ_HEADER
 
-// there are better ways of checking for this which will catch more compilers
-#if __cplusplus >= 201103L
-
-#define CAJ_CPP11
 #define MOVE(x) std::move(x)
 
 #define DEFAULT_MOVE_COPY_CONST_ASSIGN(C) \
@@ -12,12 +8,5 @@
   C(C&&) = default; \
   C& operator=(const C&) & = default; \
   C& operator=(C&&) & = default;
-
-#else
-
-#define MOVE(x) x
-
-#endif
-
 
 #endif
