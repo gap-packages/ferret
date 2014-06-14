106e30eaca221ce009a84cb9a3923df1162a0966
Chris Jefferson <caj21@st-andrews.ac.uk>
2014-06-14 18:17:53 +0100

    Start timing out graph comparison

--- scripts/check_graph_syms.sh
+++ scripts/check_graph_syms.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
+# Magic for tmpdir
+
+mytmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
+
+(ulimit -t 30; $(dirname -- "$0")/../simple_graph $1 ) > $mytmpdir/ferret 2>&1
+
+ret=$?
+
+if [ "$ret" -ne "0" ]; then
+  echo "Ferret timeout/fail: "$ret" "$1
+  exit 0
+fi
 
 if ! $(dirname -- "$0")/check_groups_equal.sh \
-   <( $(dirname -- "$0")/../simple_graph $1) \
+   $mytmpdir/ferret \
    <( $(dirname -- "$0")/gapbliss.sh $1); then
    echo Graph comparison failed: $1
    exit 1
+else
+   echo Graphs equal: $1
+   exit 0
 fi
