9f3548535c07d9e6dec5e68315c881fa8308f38d
Chris Jefferson <caj21@st-andrews.ac.uk>
2014-06-14 23:38:26 +0100

    Print summation of graph testing

--- tst/graphs/test_all_graphs.sh
+++ tst/graphs/test_all_graphs.sh
@@ -6,6 +6,11 @@ if ! [ -d graph-dist ]; then
   ./download_graphs.sh
 fi
 
-for i in $(cat graph-dist/ferret-fast.txt); do
+for i in $(cat graph-dist/bliss-fast.txt); do
     echo ../../scripts/check_graph_syms.sh graph-dist/undirected_dim/$i
-done | parallel -j4
+done | parallel -j4 | tee graph-results.txt
+
+echo $(wc -l graph-results.txt) graphs
+echo $(grep 'Graphs equal' graph-results.txt | wc -l) passes
+echo $(grep 'Ferret timeout' graph-results.txt | wc -l) timeouts
+echo $(grep 'Graph comparison failed' graph-results.txt | wc -l) failures
