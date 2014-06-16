ca474d1aaeb65f745388f1197ae0bc11c43e3327
Chris Jefferson <caj21@st-andrews.ac.uk>
2014-06-16 15:52:13 +0100

    Add undirected graph test

---
+++ YAPB++/tests/solve_cycle_problem_undirected.cc
@@ -0,0 +1,18 @@
+#include "problem.hpp"
+#include "constraints/slowgraph.hpp"
+#include "search.hpp"
+#include <iostream>
+
+int main(void)
+{
+    Problem p(6);
+    vec1<vec1<int> > s(6); // c++11 : {{2},{3},{1},{5},{6},{4}};
+    int vals[] = {0,2,3,1,5,6,4};
+    for(int i = 1; i <= 6; ++i)
+    	s[i].push_back(vals[i]);
+    p.addConstraint(new SlowGraph<GraphDirected_no>(s, &p.p_stack));
+    SearchOptions so;
+    SolutionStore ss = doSearch(&p, so);
+
+    D_ASSERT(ss.sols().size() ==(3*2)*(3*2)*2);
+}
