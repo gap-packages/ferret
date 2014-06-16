507229a5745dd4d7b893a83b72d5e57c3073acf5
Chris Jefferson <caj21@st-andrews.ac.uk>
2014-06-16 12:51:20 +0100

    Increase testing level in tests

--- YAPB++/tests/do_single_test.sh
+++ YAPB++/tests/do_single_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . ../../ferret.vars
 
-$CXX $1 -I ../source $DEBUGFLAGS -Wall -Wextra -std=gnu++11  -o $1.exe && $VALGRIND ./$1.exe && rm -r $1.exe*
+$CXX $1 -I ../source $DEBUGFLAGS -g -DDEBUG_LEVEL=2 -Wall -Wextra -std=gnu++11  -o $1.exe && $VALGRIND ./$1.exe && rm -r $1.exe*
