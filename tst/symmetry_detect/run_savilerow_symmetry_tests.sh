40e21739380fde35bbc1143a3d9507672b35ad3a
Chris Jefferson <caj21@st-andrews.ac.uk>
2014-08-13 14:09:05 +0100

    Update tests

--- tst/symmetry_detect/run_savilerow_symmetry_tests.sh
+++ tst/symmetry_detect/run_savilerow_symmetry_tests.sh
@@ -3,7 +3,7 @@ passes=0
 fails=0
 for x in savilerow_symmetry_tests/*.eprime
 do
-	./symmetry_detect_test.sh -eprime $x ${x}.expectedGroup.json
+	./symmetry_detect_test.sh -eprime $x savilerow_symmetry_tests/$(basename $x .eprime).expectedGroup.json
 	if [ $? -eq 0 ] ; then
 		((passes += 1))
 	else
--- tst/symmetry_detect/symmetry_detect_test.sh
+++ tst/symmetry_detect/symmetry_detect_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#validate arguments 
+#validate arguments
 usage='usage:  [-eprime eprime_file]|[-json json_graph] permutation_group_json_file'
 if [ $# -ne 3 ] ; then
 	echo $usage
@@ -11,24 +11,22 @@ tempfile=".symmetry_tester_temp_file"
 #run savilerow if eprime symmetry detect if json, exit if invalid args
 if [ "$1" = "-json" ] ; then
 	symmetry_detect "$2" -q > $tempfile  && $(dirname $0)/jsonToGap.py $tempfile  "$3" | gap.sh -q | grep 'true' > /dev/null
+	status=$?
 elif [ "$1" = "-eprime" ]  ; then
 	savilerow "$2" -json > /dev/null
 	if [ $? -ne 0 ] ; then
 		echo "error in running savilerow on input file $2"
 		exit 2
-		fi
+	fi
 	json=${2}.json
-		symmetry_detect "$json" -q > $tempfile  && $(dirname $0)/jsonToGap.py $tempfile  "$3" | tee test.gap |  gap.sh -q | grep 'true' > /dev/null
+	symmetry_detect "$json" -q > $tempfile  && $(dirname $0)/jsonToGap.py $tempfile  "$3" | tee test.gap |  gap.sh -q | grep 'true' > /dev/null
+	status=$?
+	rm -f ${2}.aux ${2}.json ${2}.minion
 	else
 		echo $usage
 		exit 1
 fi
 
-
-
-
-
-status=$?
 if [ $status -eq 0 ] ; then
 	echo "passed for input file $2 compared against permutation group $3."
 else
@@ -40,5 +38,5 @@ else
 		echo "--------"
 fi
 
-rm $tempfile 
-exit $status 
+rm $tempfile
+exit $status
