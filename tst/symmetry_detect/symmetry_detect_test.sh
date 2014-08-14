49b5561d8eac3c01aeb0501bf60dfb2632e728ec
Chris Jefferson <caj21@st-andrews.ac.uk>
2014-08-14 12:19:28 +0100

    Improve testing script

--- tst/symmetry_detect/symmetry_detect_test.sh
+++ tst/symmetry_detect/symmetry_detect_test.sh
@@ -10,7 +10,8 @@ fi
 tempfile=".symmetry_tester_temp_file"
 #run savilerow if eprime symmetry detect if json, exit if invalid args
 if [ "$1" = "-json" ] ; then
-	symmetry_detect "$2" -q > $tempfile  && $(dirname $0)/jsonToGap.py $tempfile  "$3" | gap.sh -q | grep 'true' > /dev/null
+	json=${2}
+	 $(dirname $0)/jsonToGap.py <(symmetry_detect "$json" -q)  "$3" | gap.sh -q | grep 'true' > /dev/null
 	status=$?
 elif [ "$1" = "-eprime" ]  ; then
 	savilerow "$2" -json > /dev/null
@@ -19,9 +20,8 @@ elif [ "$1" = "-eprime" ]  ; then
 		exit 2
 	fi
 	json=${2}.json
-	symmetry_detect "$json" -q > $tempfile  && $(dirname $0)/jsonToGap.py $tempfile  "$3" | tee test.gap |  gap.sh -q | grep 'true' > /dev/null
+	$(dirname $0)/jsonToGap.py <(symmetry_detect "$json" -q) "$3" | gap.sh -q | grep 'true' > /dev/null
 	status=$?
-	rm -f ${2}.aux ${2}.json ${2}.minion
 	else
 		echo $usage
 		exit 1
@@ -29,14 +29,16 @@ fi
 
 if [ $status -eq 0 ] ; then
 	echo "passed for input file $2 compared against permutation group $3."
+	if [ "$1" = "-eprime" ] ; then
+		rm -f ${2}.aux ${2}.json ${2}.minion
+	fi
 else
 	echo "failed test with input file $2 Compared against permutation group $3."
 	echo "symmetry groups being compared:"
-	cat $tempfile
+	symmetry_detect "$json" -q
 	echo "--------"
 	cat "$3"
 		echo "--------"
 fi
 
-rm $tempfile
 exit $status
