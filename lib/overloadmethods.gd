48b6dd908df5c9cb45d3a1d21645e434509f8c94
Chris Jefferson <caj21@st-andrews.ac.uk>
2014-10-13 14:09:44 +0100

    Add missing files

---
+++ lib/overloadmethods.gd
@@ -0,0 +1,30 @@
+#############################################################################
+##
+##
+#W  overloadmethods.gd    ferret Package                  Chris Jefferson
+##
+##  Replace built in GAP methods.
+##
+#Y  Copyright (C) 2014     University of St. Andrews, North Haugh,
+#Y                          St. Andrews, Fife KY16 9SS, Scotland
+##
+
+
+#############################################################################
+##  <#GAPDoc Label="EnableFerretOverloads">
+##  <ManSection>
+##  <Func Name="EnableFerretOverloads" Arg="[active]"/>
+##
+##  <Description>
+##  if <A>active</A> (a bool) is true, then enable
+##  Ferret specialisations of Intersection and Stabilizer.
+##  Call with <A>active</A> false to disable.
+##  </Description>
+##  </ManSection>
+##  <#/GAPDoc>
+DeclareGlobalFunction( "EnableFerretOverloads" );
+
+
+
+
+#E  files.gd  . . . . . . . . . . . . . . . . . . . . . . . . . . . ends here
