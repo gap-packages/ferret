b662973226b311cfd68f80064171db9781bd404d
Chris Jefferson <caj21@st-andrews.ac.uk>
2014-10-30 10:42:13 +0000

    Rearrange files and cleanup for next release

--- nongap-tst/graphs/test_all_graphs.sh
+++ nongap-tst/graphs/test_all_graphs.sh
@@ -12,10 +12,12 @@ if ! [ -d graph-dist ]; then
 fi
 
 for i in $(cat graph-dist/bliss-fast.txt); do
-    echo ../../scripts/check_graph_syms.sh graph-dist/undirected_dim/$i
+    echo ../scripts/check_graph_syms.sh graph-dist/undirected_dim/$i
 done | parallel -j4 | tee graph-results.txt
 
 echo $(wc -l graph-results.txt) graphs
 echo $(grep 'Graphs equal' graph-results.txt | wc -l) passes
 echo $(grep 'Ferret timeout' graph-results.txt | wc -l) timeouts
 echo $(grep 'Graph comparison failed' graph-results.txt | wc -l) failures
+
+return $(grep 'Graph comparison failed' graph-results.txt | wc -l)
