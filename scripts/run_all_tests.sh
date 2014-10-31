db27eeee21c83f6b4b487bbf06238f1399d864c7
Chris Jefferson <caj21@st-andrews.ac.uk>
2014-10-31 14:13:36 +0000

    fix path typo

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -11,7 +11,7 @@ for j in "CHECK=0" "CHECK=1"; do
   echo 'Read("tst/testall.g");' | ${GAPEXEC} -q
 
   if [ "X$VALGRIND" != "X" ]; then
-    (cd tst && echo 'LoadPackage(\"io\"); if Test(\"testvalgrind.tst\") = true then IO_exit(0); else IO_exit(1); fi;' | $VALGRIND -q --trace-children=yes --suppressions=scripts/gap-suppressions.valgrind ${GAPEXEC} -q)
+    (cd tst && echo 'LoadPackage(\"io\"); if Test(\"testvalgrind.tst\") = true then IO_exit(0); else IO_exit(1); fi;' | $VALGRIND -q --trace-children=yes --suppressions=../scripts/gap-suppressions.valgrind ${GAPEXEC} -q)
   else
     echo "echo Skipping valgrind tests"
   fi;
