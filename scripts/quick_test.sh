72225d49338337357cfdd26ecdd242935af66fe8
Chris Jefferson <caj21@st-andrews.ac.uk>
2014-11-12 14:44:11 +0000

    Improve testing

--- scripts/quick_test.sh
+++ scripts/quick_test.sh
@@ -4,9 +4,22 @@ set -o pipefail
 cd $(dirname $0)
 cd ..
 . ./ferret.vars
+
+if [ $# -eq 0 ]
+then
+  MAKEFLAGS="CHECK=1"
+else
+  MAKEFLAGS="$*"
+fi
+
 (cd YAPB++/tests && ./run_tests.sh)
 
-for j in "CHECK=1"; do
-  make $j > /dev/null
-  echo 'Read("tst/testall.g");' | ${GAPEXEC} -q
-done
+
+make ${MAKEFLAGS} > /dev/null
+echo 'Read("tst/testall.g");' | ${GAPEXEC} -q
+
+if [ "X$VALGRIND" != "X" ]; then
+  (cd tst && echo 'LoadPackage(\"io\"); if Test(\"testvalgrind.tst\") = true then IO_exit(0); else IO_exit(1); fi;' | $VALGRIND -q --trace-children=yes --suppressions=../scripts/gap-suppressions.valgrind ${GAPEXEC} -q)
+else
+  echo "echo Skipping valgrind tests"
+fi;
--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -4,17 +4,9 @@ set -o pipefail
 cd $(dirname $0)
 cd ..
 . ./ferret.vars
-(cd YAPB++/tests && ./run_tests.sh)
 
 for j in "CHECK=0" "CHECK=1"; do
-  make all $j > /dev/null
-  echo 'Read("tst/testall.g");' | ${GAPEXEC} -q
-
-  if [ "X$VALGRIND" != "X" ]; then
-    (cd tst && echo 'LoadPackage(\"io\"); if Test(\"testvalgrind.tst\") = true then IO_exit(0); else IO_exit(1); fi;' | $VALGRIND -q --trace-children=yes --suppressions=../scripts/gap-suppressions.valgrind ${GAPEXEC} -q)
-  else
-    echo "echo Skipping valgrind tests"
-  fi;
+  scripts/quick_test.sh $j
 
   (cd nongap-tst/graphs && ./test_all_graphs.sh)
 done
