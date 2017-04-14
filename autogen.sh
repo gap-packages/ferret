93118647546a6c2df4343e73dcaa4ac6f46151fe
Chris Jefferson <caj21@st-andrews.ac.uk>
2017-04-14 11:24:19 +0100

    Switch to new build system

---
+++ autogen.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -ex
+#
+# ferret: 
+#
+# This file is part of the build system of a GAP kernel extension.
+# Requires GNU autoconf, GNU automake and GNU libtool.
+#
+autoreconf -vif `dirname "$0"`
--- makedoc.g
+++ makedoc.g
@@ -5,3 +5,5 @@ if fail = LoadPackage("AutoDoc", ">= 2016.01.21") then
 fi;
 
 AutoDoc(rec(scaffold := rec( MainPage := false )));
+
+PrintTo("VERSION", PackageInfo("ferret")[1].Version);
