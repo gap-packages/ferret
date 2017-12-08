2d828e17724f93acab740100021740d6e16ae462
Max Horn <max@quendi.de>
2017-12-08 10:18:19 +0100

    Remove commented out code and misleading comments

--- src/hellod.cc
+++ src/hellod.cc
@@ -1,24 +1,5 @@
 /****************************************************************************
 
-This is a prototype of a HELLO_WORLD example implemented as a kernel module.
-
-An example of its compilation:
-
-dyn-194-155:src alexk$ ~/gap4r4/bin/i686-apple-darwin10.4.0-gcc/gac -d hellod.c
-gcc -fPIC -o /var/folders/NB/NBxYeZaRHxGpf4haob7SqE+++TQ/-Tmp-//gac36146/36146_hellod.o -I/Users/alexk/gap4r4/bin/i686-apple-darwin10.4.0-gcc/../.. -I/Users/alexk/gap4r4/bin/i686-apple-darwin10.4.0-gcc -DCONFIG_H -c hellod.c
-ld -bundle -bundle_loader /Users/alexk/gap4r4/bin/i686-apple-darwin10.4.0-gcc/gap /usr/lib/bundle1.o -lc -lm -o hellod.so /var/folders/NB/NBxYeZaRHxGpf4haob7SqE+++TQ/-Tmp-//gac36146/36146_hellod.o
-rm -f /var/folders/NB/NBxYeZaRHxGpf4haob7SqE+++TQ/-Tmp-//gac36146/36146_hellod.o
-
-It works as follows:
-
-gap> LoadDynamicModule("hellod.so");
-gap> HELLO_WORLD();
-Hello World!
-gap>
-
-It may be included in one of the future versions of the Example package.
-However, this will require documenting more rules of kernel programming.
-
 ****************************************************************************/
 
 #include <stdio.h>
@@ -28,97 +9,6 @@ However, this will require documenting more rules of kernel programming.
 
 #define FUNOBJ(x,y) GAPFunction FunObj_##x;
 #include "generated_headers/function_objs.h"
-/*
-Obj FuncHELLO_WORLD( Obj self ) {
-  Pr("Hello World!\n",0L, 0L);
-  return (Obj) 0;
-  }
-
-Obj FuncADD_ONE_SMALL_INT( Obj self, Obj arg ) {
-
-  if(!IS_INTOBJ(arg))
-  {
-    SyClearErrorNo();
-    return Fail;
-  }
-
-  int val = INT_INTOBJ(arg);
-
-  return INTOBJ_INT(val + 1);
-}
-
-Obj FuncSIMPLE_FUNCTION_CALL_WRAPPER(Obj self, Obj func, Obj arg1)
-{
-  // This just calls a function with a single argument:
-  if(!IS_FUNC(func))
-  {
-    SyClearErrorNo();
-    return Fail;
-  }
-  Obj ret = CALL_1ARGS(func, arg1);
-  return ret;
-}
-
-Obj FuncMAKE_EXAMPLE_LIST( Obj self ) {
-  // This only allocates space for the list
-  Obj list = NEW_PLIST(T_PLIST_DENSE, 5);
-  // This sets the actual length of the list
-  SET_LEN_PLIST(list, 5);
-  int i;
-  for(i = 1; i <= 5; ++i)
-    SET_ELM_PLIST(list, i, INTOBJ_INT(i));
-  return list;
-}
-
-Obj FuncMAKE_EXAMPLE_LIST_LIST( Obj self ) {
-  // This only allocates space for the list
-  Obj list = NEW_PLIST(T_PLIST_DENSE, 5);
-  // This sets the actual length of the list
-  SET_LEN_PLIST(list, 5);
-  int i;
-  for(i = 1; i <= 5; ++i)
-  {
-    Obj newlist = NEW_PLIST(T_PLIST_DENSE, 2);
-    SET_LEN_PLIST(newlist, 2);
-    SET_ELM_PLIST(newlist, 1, INTOBJ_INT(i*10));
-    SET_ELM_PLIST(newlist, 2, INTOBJ_INT(i*20));
-    SET_ELM_PLIST(list, i, newlist);
-    // (TOCHECK)
-    // We have to call CHANGED_BAG before we exit
-    // this loop, or a garbage collection could lose
-    // newlist!
-    CHANGED_BAG(list);
-  }
-  return list;
-}
-
-Obj FuncRECORD_FUN(Obj self, Obj arg) {
-  // Check arg is a record
-  if(!IS_PREC_REP(arg))
-  {
-    SyClearErrorNo();
-    return Fail;
-  }
-
-  // make a new record
-  Obj rec = NEW_PREC(0);
-
-  // set a member
-  AssPRec(rec, RNamName("one"), INTOBJ_INT(1));
-
-  // check if record has memory called 'in'
-  if(IsbPRec(arg, RNamName("inval")))
-  {
-    // Read the member out
-    Obj recval = ElmPRec(arg, RNamName("inval"));
-    // and assign it in a different record
-    AssPRec(rec, RNamName("outval"), recval);
-  }
-
-  CHANGED_BAG(rec);
-  return rec;
-}
-*/
 
 extern "C" {
 
@@ -143,14 +33,6 @@ Obj FuncSOLVE_COSET(Obj self, Obj conlistC, Obj conlistL, Obj conlistR, Obj opti
 }
 
 static StructGVarFunc GVarFuncs [] = {
-/*
-{ "HELLO_WORLD", 0, "", FuncHELLO_WORLD, "src/string.c:FuncHELLO_WORLD" },
-{ "ADD_ONE_SMALL_INT", 1, "list", FuncADD_ONE_SMALL_INT, "src/string.c:FuncADD_ONE_SMALL_INT" },
-{ "MAKE_EXAMPLE_LIST", 0, "", FuncMAKE_EXAMPLE_LIST, "src/string.c:FuncMAKE_EXAMPLE_LIST" },
-{ "MAKE_EXAMPLE_LIST_LIST", 0, "", FuncMAKE_EXAMPLE_LIST_LIST, "src/string.c:FuncMAKE_EXAMPLE_LIST_LIST" },
-{ "RECORD_FUN", 1, "record", FuncRECORD_FUN, "src/string.c:FuncRECORD_FUN" },
-{ "SIMPLE_FUNCTION_CALL_WRAPPER", 2, "function, object", FuncSIMPLE_FUNCTION_CALL_WRAPPER, "src/string.c:FuncSIMPLE_FUNCTION_CALL_WRAPPER" },
-*/
   { "YAPB_SOLVE", 2, "object, object", (UInt**(*)())FuncSOLVE, "src/string.c:FuncSOLVE" },
   { "YAPB_SOLVE_COSET", 4, "object, object, object, object", (UInt**(*)())FuncSOLVE_COSET, "src/string.c:FuncSOLVE_COSET" },
 
@@ -160,7 +42,6 @@ static StructGVarFunc GVarFuncs [] = {
 
 
 /**************************************************************************
-
 *F  InitKernel( <module> )  . . . . . . . . initialise kernel data structures
 */
 static Int InitKernel (
